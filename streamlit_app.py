16c67266bcae94774fde36531b020b5d67225bac
ts4rebels <TS4RebelsStaff@gmail.com>
2024-01-11 20:44:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
+st.title('Latest Updates')
+
 for row in df.itertuples():
     st.write(f"**{row.filename}** was uploaded on {row.date}")
