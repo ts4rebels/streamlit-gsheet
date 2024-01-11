42a4c509df3e2fed6a2c0539c22de34bc315b2ad
ts4rebels <TS4RebelsStaff@gmail.com>
2024-01-11 22:22:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
 
 for row in df[::-1].head(20).itertuples():
-    st.write(f"**{row.filename}** was uploaded on *{row.date}* - [{row.previews}](preview)")
+    st.write(f"**{row.filename}** was uploaded on *{row.date}*") if {row.previews} != nan st.markdown( - [{row.previews}](preview))
