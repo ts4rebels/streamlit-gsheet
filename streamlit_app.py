6f854420bfa7bf23201adcf9837802ad6277e69c
ts4rebels <TS4RebelsStaff@gmail.com>
2024-01-11 20:36:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-# Print results.
 for row in df.itertuples():
-    st.write(f"{row.filrname} was uploaded on :{row.date}:")
+    st.write(f"{row.filename} was uploaded on :{row.date}:")
